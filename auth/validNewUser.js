1f5e4828cc51366178a0c20119324502403b7dd5
eralpkor <eralp.ny@gmail.com>
2021-03-08 13:29:03 -0500

    Add validation for a new user functionality

--- auth/validNewUser.js
+++ auth/validNewUser.js
@@ -1,45 +1,73 @@
-const Users = require('./middleware/auth-model');
+const Users = require("./middleware/auth-model");
 
-const { validationResult, check, checkSchema, body } = require('express-validator');
-
-exports.checkUser = async(req, res, next) => {
+module.exports = async (req, res, next) => {
   const errors = [];
+
+  const validateNewUser = (user) => {
+    !user.username && errors.push({ username: "Username is required." });
+    !user.password && errors.push({ password: "Password is required." });
+    !user.email && errors.push({ email: "Email is required." });
+
+    Object.keys(user).map((u) => {
+      if (u === "password" || u === "username" || u === "email") {
+        const key = user[u].length;
+
+        // verify len min
+        if (key < 4 && u) {
+          errors.push({ [u]: "Must be a minimum of 5 characters." });
+        }
+        // verify len max
+        if (key > 50 && u) {
+          errors.push({ [u]: "Must be a maximum of 50 chars" });
+        }
+        //Validate Email Pattern
+        if (u === "email") {
+          !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user[u]) &&
+            errors.push({ error: "Unexpected Email Address" });
+        }
+      }
+    });
+  };
+
+  // validate user input
+  validateNewUser(req.body);
+
   if (!errors.length) {
-    await Users.findByName(req.body.username)
-      .then(u => u && errors.push({ msg: 'Username already exist', value: u.username, where: 'body', param: 'username'}));
-    await Users.findByEmail(req.body.email)
-      .then(e => e && errors.push({ msg: 'Email is in use.', value: e.email, where: 'body', param: 'email'}))
+    // check if user in database
+    await Users.findByName(req.body.username).then(
+      (user) => {
+        console.log(user, "Name exoist")
+        user && errors.push({ username: "Username Already Exists!" })
+      });
+    await Users.findByEmail(req.body.email).then(
+      (email) => email && errors.push({ email: "Email Already Exist!" })
+    );
   }
-  errors.length < 1 ? next() : res.status(409).json({ errors: errors })
-}
-  
-exports.registerValidator = () => {
-  return [
-    check('email')
-      .isEmail()
-      .normalizeEmail(),
-    check('username')
-      .notEmpty()
-      .withMessage('username is required')
-      .isLength({ min: 2 })
-      .withMessage('Username must be 2 characters')
-      .not()
-      .custom((val) => /[^A-za-z0-9\s]/g.test(val))
-      .withMessage('Username not use uniq characters'),
-    check('password')
-      .notEmpty()
-      .withMessage('password is required')
-      .isLength({ min: 8 })
-      .withMessage('password must be 8 characters')
-  ]
-}
 
+   // OK we are probably safe to move on send conflict error
+   errors.length < 1 ? next() : res.status(409).json({ errors: errors });
+};
 
-exports.loginValidator = () => {
-  return [
-    check('username').notEmpty().withMessage('username or email is required'),
-    check('password').notEmpty().withMessage('password is required')
-  ]
-}
+// exports.registerValidator = () => {
+//   return [
+//     check('email')
+//       .notEmpty().withMessage('Email cannot be empty')
+//       .isEmail()
+//       .normalizeEmail(),
+//     check('username')
+//       .notEmpty()
+//       .withMessage('username is required')
+//       .isLength({ min: 2 })
+//       .withMessage('Username must be 2 characters')
+//       .not()
+//       .custom((val) => /[^A-za-z0-9\s]/g.test(val))
+//       .withMessage('Username not use uniq characters'),
+//     check('password')
+//       .notEmpty()
+//       .withMessage('password is required')
+//       .isLength({ min: 8 })
+//       .withMessage('password must be 8 characters')
+//   ]
+// }
 
 // EOF
